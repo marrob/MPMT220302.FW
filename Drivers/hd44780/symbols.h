2deccf7dfcd7b9d556fafb660ab14a68883cd410
Margit Robert <marrob50hz@gmail.com>
2022-04-12 18:29:07 +0200

    symbols added

---
+++ Drivers/hd44780/symbols.h
@@ -0,0 +1,24 @@
+/******************** (C) COPYRIGHT 2013 marrob Design *************************
+* File Name          : main.c
+* Author             : Margit Róbert
+* Date First Issued  : 28/06/2013
+* Description        : 
+********************************************************************************/
+#ifndef _HD44780_SYMBOL__H_
+#define _HD44780_SYMBOL__H_ 1
+
+extern BYTE ALPHA_SYMBOL_GPS[];
+extern BYTE ALPHA_SYMBOL_PLUG[];
+extern BYTE ALPHA_SYMBOL_PPUL[];
+extern BYTE ALPHA_SYMBOL_NPUL[];
+extern BYTE ALPHA_SYMBOL_DOWN[];
+extern BYTE ALPHA_SYMBOL_UP[];
+extern BYTE ALPHA_SYMBOL_BIDIR[];
+extern BYTE ALPHA_SYMBOL_BAT_100[];
+extern BYTE ALPHA_SYMBOL_BAT_80[];
+extern BYTE ALPHA_SYMBOL_BAT_60[];
+extern BYTE ALPHA_SYMBOL_BAT_40[];
+extern BYTE ALPHA_SYMBOL_BAT_20[];
+extern BYTE ALPHA_SYMBOL_BAT_0[];
+
+#endif //_HD44780_SYMBOL__H_
